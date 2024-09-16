0a6cf3f973bfd174c28e9f8f5ceb695461cb2e48
Nikita Menshov <nikitatop2222@gmail.com>
2024-09-16 14:23:27 +0500

    Add admin panel

--- app/test/models.py
+++ app/test/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=150)
-    discription = models.CharField(max_length=300, blank=True)
+    description = models.CharField(max_length=300, blank=True)
     date = models.DateField(auto_now_add=True)
     done = models.BooleanField(default=False)
 
